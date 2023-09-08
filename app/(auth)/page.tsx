b35c5193aa0531af84b718870cef1d8eada0b896
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-08 13:53:35 -0500

    add landing headers and app details

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,7 +1,27 @@
+import AuthForm from "@/components/AuthForm";
+import Image from "next/image";
+
 function Home() {
   return (
-    <div className="flex h-screen items-center justify-center font-bold text-blue-500">
-      <p>Hello Home page</p>
+    <div className="bg-gray-10 relative flex min-h-full flex-col justify-center bg-zinc-200 py-12 sm:px-6 lg:px-8">
+      <h1 className="text-darkText absolute left-0 top-0 p-5 text-3xl font-semibold">
+        Course Craft
+      </h1>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="flex flex-row items-center justify-center gap-1">
+          <Image alt="logo" height={65} width={65} src="/assets/logo.png" />
+        </div>
+        <h2 className="tracking-light text-darkText mt-4 text-center text-3xl font-semibold">
+          Learn through the Power of AI
+        </h2>
+        <h5 className="text-darkText text-center text-lg font-bold">
+          Generate courses. Master concepts.
+        </h5>
+        <p className="mt-4 text-center text-sm font-light text-gray-500">
+          Ready to Learn? Sign in or create an account
+        </p>
+      </div>
+      <AuthForm />
     </div>
   );
 }
---
+++ components/AuthForm.tsx
@@ -0,0 +1,7 @@
+interface AuthFormProps {}
+
+function AuthForm({}: AuthFormProps) {
+  return <div></div>;
+}
+
+export default AuthForm;
