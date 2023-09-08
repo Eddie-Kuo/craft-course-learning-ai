df65697547f1842a2a02e1ec52b268300003983c
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-08 12:58:11 -0500

    install tw prettier plugin

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,9 +1,6 @@
 function Home() {
   return (
-    <div
-      className='h-screen flex items-center justify-center font-bold text-blue-500
-'
-    >
+    <div className="flex h-screen items-center justify-center font-bold text-blue-500">
       <p>Hello Home page</p>
     </div>
   );
