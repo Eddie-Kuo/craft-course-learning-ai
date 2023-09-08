8fd233c3495b1ac5078628c00fb908a69ed1aab6
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-08 15:52:25 -0500

    init auth form zod schema

---
+++ lib/validations/auth.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import * as z from "zod";
+
+const authFormSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters long"),
+  email: z.string().email("Please enter a valid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export default authFormSchema;
