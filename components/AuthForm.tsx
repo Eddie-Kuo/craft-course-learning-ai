e3879ed43cffcd717f30cbb5146257bcffcd04f5
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-08 16:08:19 -0500

    init building of auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,7 +1,41 @@
-interface AuthFormProps {}
+"use client";
 
-function AuthForm({}: AuthFormProps) {
-  return <div></div>;
+import authFormSchema from "@/lib/validations/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Form } from "./ui/form";
+
+type AuthInput = z.infer<typeof authFormSchema>;
+
+function AuthForm() {
+  // define the form
+  const form = useForm<AuthInput>({
+    resolver: zodResolver(authFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  // handle form submission
+  const onSubmit = (data: AuthInput) => {
+    // TODO: handle form submission
+  };
+
+  return (
+    <div className="mx-auto mt-8 sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6"
+          ></form>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default AuthForm;
