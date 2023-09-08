d471ee07830c1019fda10f8783dcdb194fb34b40
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-08 12:54:47 -0500

    init auth page

---
+++ app/(auth)/page.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div
+      className='h-screen flex items-center justify-center font-bold text-blue-500
+'
+    >
+      <p>Hello Home page</p>
+    </div>
+  );
+}
+
+export default Home;
