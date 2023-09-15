2987cc4cb80be721916e829c51a4f1608d15006b
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-15 18:25:44 -0500

    add shadcn dropdown menu + init profile options

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,8 +1,33 @@
 "use client";
+
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
 interface AvatarProps {}
 
 function Avatar({}: AvatarProps) {
-  return <div>Avatar</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button>User</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <div className="flex items-center justify-center gap-2 p-2">
+          <div>
+            <p>Name</p>
+            <p>Email</p>
+            <p>Available Credits:</p>
+            <p>Settings</p>
+            <p>Sign Out</p>
+          </div>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default Avatar;
