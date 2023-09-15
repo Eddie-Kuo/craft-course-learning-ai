893af1dcdab013e0db8972dd509b168f22dc0a75
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-15 18:08:09 -0500

    init header nav links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,11 +5,16 @@ interface NavbarProps {}
 
 function Navbar({}: NavbarProps) {
   return (
-    <nav className="fixed top-0 z-30 flex w-full items-center justify-between bg-blue-100 px-6 py-4 ">
+    <nav className="fixed top-0 z-30 flex w-full items-center justify-between border border-b-zinc-600/30 bg-blue-100 px-6 py-4">
       <Link href="/" className="flex items-center gap-4">
         <Image alt="logo" height={65} width={65} src="/assets/logo.png" />
         <p>Craft Course Learning</p>
       </Link>
+
+      <div className="flex items-center gap-3">
+        <Link href="/">Dashboard</Link>
+        <Link href="/create">Create Course</Link>
+      </div>
     </nav>
   );
 }
