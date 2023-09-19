eca82b989e879cf98d19df953d170251e48e14ad
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-19 15:50:14 -0500

    set up react forms for chapter creation

--- components/CreateCourseForm.tsx
+++ components/CreateCourseForm.tsx
@@ -1,9 +1,29 @@
 "use client";
 
-interface CreateCourseFormProps {}
+import createChapterSchema from "@/lib/validations/course";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "./ui/form";
 
-function CreateCourseForm({}: CreateCourseFormProps) {
-  return <div className="w-full">CreateCourseForm</div>;
+type CourseInput = z.infer<typeof createChapterSchema>;
+
+function CreateCourseForm() {
+  const form = useForm<CourseInput>({
+    resolver: zodResolver(createChapterSchema),
+    defaultValues: {
+      title: "",
+      units: ["", "", ""],
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <Form {...form}>
+        <form></form>
+      </Form>
+    </div>
+  );
 }
 
 export default CreateCourseForm;
