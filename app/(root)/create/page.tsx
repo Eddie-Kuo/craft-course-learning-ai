d39d262de7ca572cbf7f24f7e4d893eb14d6dd6e
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-19 16:54:18 -0500

    init inputs for course creation

--- app/(root)/create/page.tsx
+++ app/(root)/create/page.tsx
@@ -8,11 +8,11 @@ async function CreatePage({}: pageProps) {
   const session = await getSession();
 
   return (
-    <div className="mx-auto flex h-screen max-w-3xl flex-col items-center justify-center px-8">
+    <div className="mx-auto flex h-screen max-w-3xl flex-col items-center justify-center gap-4 px-8">
       <h1 className="text-center text-2xl font-bold text-darkText sm:text-4xl">
         Craft Course Learning AI
       </h1>
-      <div className="mt-4 flex gap-3 rounded-md border border-slate-100 bg-zinc-400/70 p-4">
+      <div className="mt-2 flex gap-3 rounded-md border border-slate-100 bg-zinc-400/70 p-4">
         <AiOutlineInfoCircle className="h-20 w-20 " />
         <div>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
--- components/CreateCourseForm.tsx
+++ components/CreateCourseForm.tsx
@@ -4,7 +4,8 @@ import createChapterSchema from "@/lib/validations/course";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Form } from "./ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { Input } from "./ui/input";
 
 type CourseInput = z.infer<typeof createChapterSchema>;
 
@@ -17,10 +18,57 @@ function CreateCourseForm() {
     },
   });
 
+  const onSubmit = (data: CourseInput) => {};
+
   return (
     <div className="w-full">
       <Form {...form}>
-        <form></form>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => {
+              return (
+                <FormItem className="flex w-full flex-col items-start sm:flex-row sm:items-center">
+                  <FormLabel className="flex-[1] px-1 text-xl font-semibold">
+                    Title
+                  </FormLabel>
+                  <FormControl className="flex-[8]">
+                    <Input
+                      placeholder=" Enter a main topic you want to learn more about"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              );
+            }}
+          />
+
+          {form.watch("units").map((_, index) => {
+            return (
+              <FormField
+                control={form.control}
+                name={`units.${index}`}
+                key={index}
+                render={({ field }) => {
+                  return (
+                    <FormItem className="flex w-full flex-col items-start sm:flex-row sm:items-center">
+                      <FormLabel className="flex-[1] px-1 text-xl font-semibold">
+                        Unit {index + 1}
+                      </FormLabel>
+                      <FormControl className="flex-[8]">
+                        <Input
+                          placeholder="Enter subtopic of choice for the course"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  );
+                }}
+              />
+            );
+          })}
+        </form>
       </Form>
     </div>
   );
