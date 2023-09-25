07cda88eec091f247abcd4a3d63ede54b7408f9e
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-25 17:01:13 -0500

    disclaimer message for failed generations

--- app/(root)/create/[courseId]/page.tsx
+++ app/(root)/create/[courseId]/page.tsx
@@ -40,9 +40,18 @@ async function CoursePage({ params: { courseId } }: pageProps) {
 
       <div className="my-2 flex items-center gap-3 rounded-md border border-slate-100 bg-zinc-400/70 p-4">
         <AiOutlineInfoCircle className="h-20 w-20 " />
-        <div>
-          We`ve generated chapters for each of your units below. Please look
-          over them before clicking the confirmation button to continue.
+        <div className="flex flex-col gap-2">
+          <p>
+            We`ve generated chapters for each of your units below. Please look
+            over them before clicking the confirmation button to continue.
+          </p>
+          <p>
+            <strong>Disclaimer:</strong> Due to the nature of the OpenAI Api &
+            Youtube Api, if a chapter fails to generate and displays red,
+            refresh the page and try generating again. If another fail occurs,
+            either continue with the loaded chapters or recreate the course with
+            new units. We apologize for the inconvenience
+          </p>
         </div>
       </div>
       <ConfirmChapters course={course} />
