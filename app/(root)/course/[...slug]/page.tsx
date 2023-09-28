ff027d45c8232a6cc0fa920609f4532fba693684
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-28 13:28:12 -0500

    init course page

---
+++ app/(root)/course/[...slug]/page.tsx
@@ -0,0 +1,41 @@
+import prisma from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface pageProps {
+  params: {
+    slug: string[];
+  };
+}
+
+async function CoursePage({ params: { slug } }: pageProps) {
+  const [courseId, unitIndexParam, chapterIndexParam] = slug;
+  const course = await prisma.course.findUnique({
+    where: { id: courseId },
+    include: {
+      units: {
+        include: { chapters: true },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/dashboard");
+  }
+
+  let unitIndex = parseInt(unitIndexParam);
+  let chapterIndex = parseInt(chapterIndexParam);
+
+  const unit = course.units[unitIndex];
+  if (!unit) {
+    return redirect("/dashboard");
+  }
+
+  const chapter = unit.chapters[chapterIndex];
+  if (!chapter) {
+    return redirect("/dashboard");
+  }
+
+  return <CourseSideBar />;
+}
+
+export default CoursePage;
