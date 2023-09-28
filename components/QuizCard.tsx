7f0874b5490abe7d34d510cf0b2965315a68245f
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-28 16:07:39 -0500

    add answer check to question

--- components/QuizCard.tsx
+++ components/QuizCard.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Chapter, Question } from "@prisma/client";
+import { useCallback, useState } from "react";
 
 interface QuizCardProps {
   chapter: Chapter & {
@@ -7,7 +9,28 @@ interface QuizCardProps {
 }
 
 function QuizCard({ chapter }: QuizCardProps) {
-  return <div>QuizCard</div>;
+  const [answer, setAnswer] = useState<Record<string, string>>({});
+  const [questionState, setQuestionState] = useState<
+    Record<string, boolean | null>
+  >({});
+
+  const checkAnswer = useCallback(() => {
+    const newQuestionState = { ...questionState };
+    chapter.questions.forEach((question) => {
+      const userAnswer = answer[question.id];
+      if (!userAnswer) return;
+
+      if (userAnswer === question.answer) {
+        newQuestionState[question.id] = true;
+      } else {
+        newQuestionState[question.id] = false;
+      }
+
+      setQuestionState(newQuestionState);
+    });
+  }, [answer, questionState, chapter.questions]);
+
+  return <div>Hello</div>;
 }
 
 export default QuizCard;
