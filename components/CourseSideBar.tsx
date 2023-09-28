31375b1b28507863b2d5a9a87fa846d5812d485a
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-09-28 13:59:57 -0500

    display unit and chapters in side bar

--- components/CourseSideBar.tsx
+++ components/CourseSideBar.tsx
@@ -1,4 +1,6 @@
 import { Chapter, Course, Unit } from "@prisma/client";
+import Link from "next/link";
+import { Separator } from "./ui/separator";
 
 interface CourseSideBarProps {
   course: Course & {
@@ -8,8 +10,34 @@ interface CourseSideBarProps {
   };
 }
 
-function CourseSideBar({ course }: CourseSideBarProps) {
-  return <div>CourseSideBar</div>;
+async function CourseSideBar({ course }: CourseSideBarProps) {
+  return (
+    <div className="absolute top-1/2 w-[400px] -translate-y-1/2 rounded-r-3xl bg-secondary p-6">
+      <h1 className="text-3xl font-semibold uppercase text-darkText">
+        {course.name}
+      </h1>
+      <Separator className="my-2 bg-zinc-400" />
+      {course.units.map((unit, unitIndex) => {
+        return (
+          <div key={unitIndex}>
+            <h2>{unit.name}</h2>
+            {unit.chapters.map((chapter, chapterIndex) => {
+              return (
+                <div key={chapterIndex}>
+                  <Link
+                    href={`/course/${course.id}/${unitIndex}/${chapterIndex}`}
+                  >
+                    {chapter.name}
+                  </Link>
+                </div>
+              );
+            })}
+            <Separator className="mt-2 bg-zinc-400" />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default CourseSideBar;
