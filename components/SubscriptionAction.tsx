ecec847896d24e1cb47ce1f6850bea801954e2ac
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-11-17 16:29:59 -0600

    add styling to subscription ui

--- components/SubscriptionAction.tsx
+++ components/SubscriptionAction.tsx
@@ -9,13 +9,14 @@ function SubscriptionAction({}: SubscriptionActionProps) {
   const { data } = useSession();
 
   return (
-    <div>
+    <div className="mx-auto mt-4 flex w-1/2 flex-col items-center gap-2 rounded-md">
       {data?.user?.credits} / 5 Free Generations
       <Progress
         value={data?.user.credits ? (data.user.credits / 5) * 100 : 0}
+        color={"blue"}
       />
-      <Button>
-        <Zap className="mr-2 " />
+      <Button className="mt-2 bg-gradient-to-tr from-slate-600 to-sky-600 font-bold text-zinc-200 transition hover:from-sky-600 hover:to-slate-600">
+        <Zap className="mr-2" />
         Upgrade to Premium
       </Button>
     </div>
