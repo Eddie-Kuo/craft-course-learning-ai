f3956f67aa53276228eaf2c8a94a8cc989171d98
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-11-18 21:00:19 -0600

    add route to generate a new session for first time subscribers

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -31,6 +31,42 @@ export async function GET() {
       return NextResponse.json({ url: stripeSession.url });
     }
 
-    // b. User's first time subscribing
-  } catch (error) {}
+    // b. User's first time subscribing; create a new stripe session for them as well
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsURL,
+      cancel_url: settingsURL,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: session.user.email ?? "",
+      line_items: [
+        {
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: "Craft Course Learning Pro",
+              description: "Unlimited course generations!",
+            },
+            unit_amount: 1500,
+            recurring: {
+              interval: "month",
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      // after the user pays at stripe, stripe will send a webhook to our api along with the user id
+      // with that, we will know exactly who paid and then we can update our database accordingly
+      metadata: {
+        userId: session.user.id,
+      },
+    });
+
+    return NextResponse.json({ url: stripeSession.url });
+  } catch (error) {
+    console.error("ERROR: Creating a Stripe Session", error);
+    return new NextResponse("ERROR: Creating a Stripe Session", {
+      status: 500,
+    });
+  }
 }
