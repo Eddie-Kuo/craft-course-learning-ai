e850d2b80b7b4e8723790efa4bd6b4d15fbf7dfa
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-11-18 20:45:06 -0600

    add route for user to manage current subscription if already subscribed

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,5 +1,9 @@
 import getSession from "@/lib/actions/getSession";
+import prisma from "@/lib/db";
+import { stripe } from "@/lib/strips";
 import { NextResponse } from "next/server";
+
+const settingsURL = process.env.NEXT_PUBLIC_URL + "/settings";
 
 export async function GET() {
   try {
@@ -10,9 +14,23 @@ export async function GET() {
     }
 
     // 1. Find the current subscription for the user
+    const userSubscription = await prisma.userSubscription.findUnique({
+      where: {
+        userId: session.user.id,
+      },
+    });
 
-    // a. User will be able to cancel their current subscription if they have one
+    // a. User is already subscribed and want to access their subscription.
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      // Create a new strip session for them to edit their subscription
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingsURL,
+      });
 
-    // b. User will be able to subscribe if they aren't already
+      return NextResponse.json({ url: stripeSession.url });
+    }
+
+    // b. User's first time subscribing
   } catch (error) {}
 }
