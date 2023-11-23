900b5878d6903e6a5026151154ee5913c1f70e03
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-11-23 13:21:50 -0600

    docker configs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com", "s3.us-west-2.amazonaws.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
