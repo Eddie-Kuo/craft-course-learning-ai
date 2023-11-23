fc7d2ac4171d158019424bf97cf22c5325834d09
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-11-23 14:09:16 -0600

    empty dash message

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -20,12 +20,18 @@ export default async function Home() {
   });
 
   return (
-    <div className=" mx-auto max-w-5xl pt-40">
+    <div className="mx-auto flex max-w-5xl justify-center pt-40">
       <div className="grid grid-cols-1 place-items-center gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {data.map((course) => {
-          return <DashboardCourseCard key={course.id} course={course} />;
-        })}
+        {data.length > 0 &&
+          data.map((course) => {
+            return <DashboardCourseCard key={course.id} course={course} />;
+          })}
       </div>
+      {data.length === 0 && (
+        <h1 className="text-2xl font-semibold">
+          Welcome to Craft Course! Start by creating a course and begin learning
+        </h1>
+      )}
     </div>
   );
 }
